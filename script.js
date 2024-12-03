9070cc4cb7125bfccbd66a8cb63a3c44f6197035
Anna <anna.r.german1410@gmail.com>
2024-12-04 00:26:45 +0300

    refactor: correct js-functions

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ form.addEventListener("submit", handleMainForm);
 let styleForm = document.querySelector("#style-block"); // форма для стилизации заголовка элемента
 let resetButtons = document.querySelectorAll("button[type='reset']");
 
-resetButtons.forEach((btn) => {
-    btn.addEventListener("click", clearFields);
-});
+resetButtons.forEach( (btn) => btn.addEventListener("click", clearFields) );
 
 // * reset -->
 function clearFields() {
@@ -22,13 +20,13 @@ function clearFields() {
     document.querySelector("#outputSectionElem").innerHTML = "";
     document.querySelector("#outputSectionElemCode").innerHTML = "";
 
-    // изменить текст и цвет кнопки "Copy"
-    copyBtn.forEach((elem) => {
-        elem.innerHTML = "Copy";
+    // изменить текст и цвет кнопки "Скопировать"
+    copyBtn.forEach( (elem) => {
+        elem.innerHTML = "Скопировать";
         elem.style.color = "#858585";
-    })
+    } );
 
-    // вернуть значения полей формы и отображение блока к установленным по умолчанию
+    // вернуть значения полей формы стилизации и отображение блока к установленным по умолчанию
     styleForm.chosenColor.value = "#000000";
     styleForm.chosenAlign.value = "left";
     document.querySelector("#outputSectionElem").style.color = "#000000";
@@ -44,9 +42,8 @@ function clearFields() {
     document.querySelector("#copy-style-block").style.display = "none";
     document.querySelector(".use-example").style.display = "none";
 
-    //скрыть нижнюю кнопку очистить поля
+    //скрыть нижнюю кнопку "Очистить поля"
     document.querySelector("#reset-btn-duplicate").style.display = "none";
-
 }
 
 // * submit (основная форма) --> 
@@ -57,36 +54,34 @@ function handleMainForm(e) {
     // --- Получение данных из формы
     let idStr = form.idStr.value; // данные для ссылки в главном меню
     let textLink = form.textLink.value;
-    let blockTag = form.blockTag.value || `h2`; // данные для раздела/секции, связанного со ссылкой-якорем
+    let blockTag = form.blockTag.value || `h2`; // данные для раздела/секции
 
     if (idStr && textLink && blockTag) {
         // как отображается основная ссылка на странице
-        let outputMainLink = displayMainLink(idStr, textLink); //вызов функции
+        let outputMainLink = makeMainLink(idStr, textLink);
         document.querySelector("#outputMainLink").innerHTML = outputMainLink;
-        //console.log(`Основная ссылка: ${outputMainLink}`);
 
-        // Код для основной ссылки
-        let outputMainLinkCode = displayStr(outputMainLink); //вызов функции
+        // код для основной ссылки
+        let outputMainLinkCode = makeCodeBlock(outputMainLink);
         document.querySelector("#outputMainLinkCode").innerHTML = outputMainLinkCode;
-        //console.log(`Код основной ссылки: ${outputMainLinkCode}`);
 
         // как отображается заголовок в блоке/секции
-        let outputSectionElem = displaySectionElem(idStr, textLink, blockTag); //вызов функции
+        let outputSectionElem = makeSectionElem(idStr, textLink, blockTag);
         document.querySelector("#outputSectionElem").innerHTML = outputSectionElem;
-        //console.log(`Блок/секция: ${outputSectionElem}`);
 
-        // Код для блока/секции
-        let outputSectionElemCode = displayStr(outputSectionElem); //вызов функции
+        // код для блока/секции
+        let outputSectionElemCode = makeCodeBlock(outputSectionElem);
         document.querySelector("#outputSectionElemCode").innerHTML = outputSectionElemCode;
 
-        // отобразить форму стилизации заголовка и формы копирования, а также пример использования стилей
+        // отобразить форму стилизации заголовка и формы копирования, 
+        // а также пример использования стилей
         styleForm.style.display = "block"; 
         document.querySelector("#copy-style-block").style.display = "block";
         document.querySelector(".use-example").style.display = "block";
 
         styleForm.addEventListener("submit", handleStyleForm); 
 
-        // отобразить нижнюю кнопку очистить стили
+        // отобразить нижнюю кнопку "Очистить стили"
         document.querySelector("#reset-btn-duplicate").style.display = "block";
 
     } else {
@@ -109,62 +104,63 @@ function handleStyleForm(e) {
     // * при клике на кнопку копирования стилей -->
     let copyStyleBtn = document.querySelector("#copy-style-btn");
     copyStyleBtn.removeAttribute("disabled"); // "разблокировка" кнопки
+    copyStyleBtn.addEventListener("click", function() { copyStyles(color, align) });
+}
 
-    copyStyleBtn.addEventListener("click", function() {
-        let outputStyles = `style="color:${color};text-align:${align};"`;
+// Функция копирования стилей при клике на кнопку
+function copyStyles(color, align) {
+    let copyStyleBtn = document.querySelector("#copy-style-btn");
+    let outputStyles = `style="color:${color};text-align:${align};"`;
 
-        navigator.clipboard.writeText(outputStyles);
-        copyStyleBtn.innerHTML = "Скопировано!"
-        copyStyleBtn.style.color = "#44bc7f";
+    copyString(copyStyleBtn, outputStyles);
 
-        // изменение текста кнопки копирования при выборе других свойств
-        styleForm.chosenColor.addEventListener("change", resetCopyBtn);
-        styleForm.chosenAlign.addEventListener("change", resetCopyBtn);
-        
-        function resetCopyBtn() {
-            copyStyleBtn.innerHTML = "Скопировать стили";
-            copyStyleBtn.style.color = "#000000";
-        }
-    });  
+    // изменение текста кнопки копирования при выборе других свойств
+    styleForm.chosenColor.addEventListener("change", resetCopyBtn);
+    styleForm.chosenAlign.addEventListener("change", resetCopyBtn);
+}  
 
+// Функция сброса текста и цвета на кнопке копирования
+function resetCopyBtn() {
+    let copyStyleBtn = document.querySelector("#copy-style-btn");
+    copyStyleBtn.innerHTML = "Скопировать стили";
+    copyStyleBtn.style.color = "#000000";
 }
 
 // * Копирование кода при клике на кнопку Copy
-const copyBtn = document.querySelectorAll('.copy-btn'); // все кнопки Copy
-
-//перебрать кнопки
-copyBtn.forEach((elem, index) => {
+const copyBtn = document.querySelectorAll('.copy-btn');
+copyBtn.forEach( (elem, index) => {
     elem.addEventListener('click', function () {
         let className = '.display-code' + index;
-        //console.log(className);
         let code = document.querySelector(className).innerHTML;
-        //console.log(code);
 
         if (code !== "") {
-            navigator.clipboard.writeText(code); // копирование
-            elem.innerHTML = "Copied";
-            elem.style.color = "#44bc7f";
+            copyString(elem, code);
         }
-    })
+    } );
+} );
 
-})
+function copyString(element, strToCopy) {
+    navigator.clipboard.writeText(strToCopy);
+    element.innerHTML = "Скопировано";
+    element.style.color = "#44bc7f";
+}
 
-// * ФУНКЦИИ
+// * ФУНКЦИИ построения ссылок, заголовка и тстроки кода
 
 // Функция построения основной ссылки
-function displayMainLink(id, text) {
+function makeMainLink(id, text) {
     return `<a href="#${id}">${text}</a> <br>`;
 }
 
-// Функция для добавления элемента в код блока/секции
-function displaySectionElem(id, text, tag) {
+// Функция построения кода заголовка блока/секции
+function makeSectionElem(id, text, tag) {
     return `<${tag} id="${id}">${text}</${tag}>`;
 }
 
-//  -- Функция, позволяющая получить строку с тегами и замененными символами < и > и отобразить ее на странице браузера
+// Функция построения блока с тегами и замененными символами < и > 
 // принимает строку непреобразованного кода
-function displayStr(string) {
-    console.log(string);
+function makeCodeBlock(string) {
+    //console.log(string);
 
     let start = `<p style="font-family:courier; font-size:14px;">`;
     let end = `</p>`;
